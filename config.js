a1b293f61dea46283d9720befa8455fe7b1f9b8b
nethumagee <visperbrain@gmail.com>
2025-03-28 13:44:07 +0530

    🎬MOVIE VISPER🎬

---
+++ config.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+
+
+module.exports = {
+
+SESSION_ID: process.env.SESSION_ID === undefined ? 'Put session id' : process.env.SESSION_ID, 
+GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN === undefined ? 'put github auth token' : process.env.GITHUB_AUTH_TOKEN,  //🛑 Github token එක දැමීමේදී ghp_  කොටස ඉවර් කර දමන්න.  
+GITHUB_USER_NAME: process.env.GITHUB_USER_NAME === undefined ? 'Github user name' : process.env.GITHUB_USER_NAME, // 🛑 Github user name එක ගද්දි ඔයාලා token එක ගත්ත Github එකේ user name එක දාන්න අනිවාරෙන්
+PORT: process.env.PORT === undefined ? '8000' : process.env.PORT, 
+
+
+//🛑 Github token එක දැමීමේදී ghp_  කොටස ඉවර් කර දමන්න.
+//🛑 Github user name එක ගද්දි ඔයාලා token එක ගත්ත Github එකේ user name එක දාන්න අනිවාරෙන්
+
+};
